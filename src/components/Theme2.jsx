b3d14259122acb99a88dd3c7af5910b5e73beb12
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2025-03-11 17:45:36 +0600

    update ll

---
+++ src/components/Theme2.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+
+const Theme = ({data}) => {
+
+
+    return (
+        <div>
+            this is theme two {data}
+        </div>
+    );
+};
+
+export default Theme;
