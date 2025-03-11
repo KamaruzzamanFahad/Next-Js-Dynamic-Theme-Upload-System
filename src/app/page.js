517e392819ed92b635158957de6987fa0f511ec2
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2025-03-11 17:10:31 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  const theme = "Theme1"; 
+  const theme = "Theme"; 
   const Theme = require(`@/components/${theme}`).default;
 
   return (
